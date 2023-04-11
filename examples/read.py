781b62bab101e87238149ca8fe56ab3329bf0bee
Maikel Punie <maikel.punie@gmail.com>
2023-04-11 20:11:34 +0200

    Initial code to get the basic send/receive working

--- examples/read.py
+++ examples/read.py
@@ -1,13 +1,20 @@
 """Basic example to read the bus."""
+import argparse
 import asyncio
 from duotecno.controller import PyDuotecno
 
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--host", help="The host to connect to")
+parser.add_argument("--port", help="The port to connect to")
+parser.add_argument("--password", help="The password")
+args = parser.parse_args()
 
-async def test():
+
+async def test(host, port, passw):
     """Basic aio call."""
     tmp = PyDuotecno()
-    await tmp.connect("host.duotecno-ip.be", 19001, "")
+    await tmp.connect(host, port, passw)
     await asyncio.sleep(6000000000)
 
 
-asyncio.run(test())
+asyncio.run(test(args.host, args.port, args.password))
